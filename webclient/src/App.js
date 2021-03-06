0d28de4d200b672aaa703860c5a66ff70a2f7d62
Eric Douglass <eric.douglass@gmail.com>
2021-03-06 12:34:56 -0800

    Quick resorting of concepts, basic planning notes added

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Store } from './context/context';
 import Keyboard from './components/Keyboard';
 import Cutscene from './components/Cutscene';
-import Landing from './pages/Landing';
+import GameScreen from './pages/GameScreen';
 
 
 const App = () => {
@@ -12,10 +12,35 @@ const App = () => {
       <Router>
         <Keyboard />
         <Cutscene />
-        <Route exact path='/' component={Landing} />
+        <Route exact path='/' component={GameScreen} />
       </Router>
     </Store>
   )
 }
 
 export default App;
+
+/*
+
+  DRAFTUS
+
+  -- This is a little tricky, because there's a big difference between "fast mode" and "ideal mode" in my mind.
+
+  Let's outline both real quick...
+  FAST MODE:
+  -- turnaround will be much more streamlined
+  -- creativity is born out of constraints; might come up with some cool results just based on being limited
+  -- can always build it out more thoroughly later, or use the concepts explored more deeply and quickly to launch other 'games' down the line
+
+  
+  IDEAL MODE:
+  -- moar graphics, which will carry over in knowledge base better with Proper PrPl
+  -- 
+
+
+  BLENDED MODE: 
+  -- if I can figure out a decent 'best of both worlds' option...?
+  
+  
+
+*/
--- webclient/src/pages/GameScreen.js
+++ webclient/src/pages/GameScreen.js
@@ -2,14 +2,14 @@ import React, { useContext } from 'react';
 import { Context } from '../context/context';
 import { PageContainer, Title } from '../components/styled';
 
-const Landing = () => {
+const GameScreen = () => {
     const [state, dispatch] = useContext(Context);
 
     return (
         <PageContainer>
-            <Title>I am HOME.</Title>
+            <Title>I am GAME SCREEN (turnt on).</Title>
         </PageContainer>
     )
 }
 
-export default Landing;
+export default GameScreen;
